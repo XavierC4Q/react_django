fcffd388ce4e808cfcd9d748d81301b641ab70e5
Xavier <munroexavier@gmail.com>
2019-02-27 21:24:31 -0500

    adjusted time stamp creation and added body field to Mood model.

--- backend/venv/app/models.py
+++ backend/venv/app/models.py
@@ -11,9 +11,10 @@ class MyUser(AbstractUser):
 class Mood(models.Model):
     user_id = models.ForeignKey(MyUser, related_name='user_id', on_delete=models.CASCADE)
     title = models.CharField(max_length=50, default='')
+    body = models.CharField(max_length=500, default='')
     primary_mood = models.CharField(max_length=50, blank=True, null=True)
     secondary_mood = models.CharField(max_length=50, blank=True, null=True)
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return 'Mood: id = {id}, user = {user_id}'.format(id=self.id, user_id=self.user_id)
